4d61ac37d61435029de6732e46ea1e53fee244b5
pfcao <pftsao@users.noreply.github.com>
2016-06-15 19:08:28 +0800

    markx

---
+++ assets/js/markx.js
@@ -0,0 +1,60 @@
+var LINE_ENDING = '\r',
+LINE_ENDING_SPLITER = /\r\n|\r|\n/
+
+var V ="\t";
+var TABREG = new RegExp('^('+ String.fromCharCode(32) +'|'+ String.fromCharCode(160) +'){4}')
+    function getLevel(line) {
+        var level = 0;
+        while (TABREG.test(line)) {
+            line = line.replace(TABREG, '');
+            level++;
+        }
+
+        return level;
+    }
+
+
+var t = $("pre > code").each(
+	function(index){
+		var e = $(this)
+		var p = e.parent()
+		u = e.text()
+		var text = e.text()
+
+
+		// .replace(/\s{2}/g, "+")
+
+		console.log(text)
+
+		// var tn = (text.match(/p/g) || []).length
+		// var tn2 = (text.match(/pp/g) || []).length
+		// tn && (tn == tn2 * 2) && (text = text.replace(/pp/g, "+") )
+		// console.log( text)
+		var t = text.split(LINE_ENDING_SPLITER)
+		t[0] || t.shift()
+		t.pop()
+
+		var r = "";
+
+		for(var i =0; i< t.length; i++){
+			console.log(t[i])
+			var ti = t[i]
+			var level = getLevel(ti)
+			var si = ti.trim()
+			ti = ti.replace(/\s/g, '&nbsp')
+
+			r+='<br/><span class="x'+level%6+'">'+ti+'</span>'
+
+		}
+
+
+
+		p.replaceWith('<div>'+r+'</div>')
+		console.log( r)
+		// console.log(this)
+
+
+	}
+
+	)
+
