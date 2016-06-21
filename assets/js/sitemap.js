822870123c210610755fa77afe2be46f8f4d1608
pfcao <pftsao@users.noreply.github.com>
2016-06-21 22:04:54 +0800

    .

--- assets/js/sitemap.js
+++ assets/js/sitemap.js
@@ -3,13 +3,22 @@ layout: nil
 permalink:
 ---
 
-window.td_sitemap = {
-{% for post in site.posts %}{{ post.title }}:'{{ post.url }}',{% endfor %}
+window._sitemap = {
+{% for post in site.posts %}'{{ post.title }}':'{{ post.url }}',{% endfor %}
 }
+
+function text_clear(text){
+  return (text||'').replace(/[\[\]\-\+\_\'\"\<\>\s]/g,'')
+}
+window.sitemap = {}
+for (var key in _sitemap){
+  sitemap[text_clear(key)] = _sitemap[key]
+}
+
 $("p code").each(
 	function(index){
 		var title = $(this).text()
-    var url = td_sitemap[title]
+    var url = sitemap[text_clear(title)]
     if(url){
 		$(this).replaceWith('<a src="'+url+'">'+title+'</a>')
   }
