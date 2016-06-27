0c183da51894aeee002d4e26095974c970fb8c1f
pfcao <pftsao@users.noreply.github.com>
2016-06-27 13:02:45 +0800

    markx `

--- assets/js/markx.js
+++ assets/js/markx.js
@@ -1,9 +1,10 @@
-(function(){
-var LINE_ENDING = '\r',
-LINE_ENDING_SPLITER = /\r\n|\r|\n/
+(function() {
+    var LINE_ENDING = '\r',
+        LINE_ENDING_SPLITER = /\r\n|\r|\n/
 
-var V ="\t";
-var TABREG = new RegExp('^('+ String.fromCharCode(32) +'|'+ String.fromCharCode(160) +'){4}')
+    var V = "\t";
+    var TABREG = new RegExp('^(' + String.fromCharCode(32) + '|' + String.fromCharCode(160) + '){4}')
+
     function getLevel(line) {
         var level = 0;
         while (TABREG.test(line)) {
@@ -14,50 +15,54 @@ var TABREG = new RegExp('^('+ String.fromCharCode(32) +'|'+ String.fromCharCode(
         return level;
     }
 
-$("pre > code").each(
-	function(index){
-		var e = $(this)
-		var p = e.parent()
-		u = e.text()
-		var text = e.text()
-    text = (text|| '')
-    .replace(/\{/g, '<b class="x-p x-p-l">｛ </b>')
-    .replace(/\}/g, '<b class="x-p x-p-r">｝</b>')
-    .replace(/([\|\[\]\(\)])/g,
-        function(x){
-          x=String.fromCharCode(x.charCodeAt(0)+65248);
-          return '<a class="x-ps">'+x+'</a>'}
-      )
+    $("pre > code").each(
+        function(index) {
+            var e = $(this)
+            var p = e.parent()
+            u = e.text()
+            var text = e.text()
+            text = (text || '')
+                .replace(/\{/g, '<b class="x-p x-p-l">｛ </b>')
+                .replace(/\}/g, '<b class="x-p x-p-r">｝</b>')
+                .replace(/([\|\[\]\(\)])/g,
+                    function(x) {
+                        x = String.fromCharCode(x.charCodeAt(0) + 65248);
+                        return '<a class="x-ps">' + x + '</a>'
+                    }
 
-		var t = text.split(LINE_ENDING_SPLITER)
-		t[0] || t.shift()
-		t.pop()
+                )
 
-		var r = "";
+            var t = text.split(LINE_ENDING_SPLITER)
+            t[0] || t.shift()
+            t.pop()
 
-		for(var i =0; i< t.length; i++){
-			// console.log(t[i])
-      if(i){r+='<br/>'}
-			var ti = t[i]
-			var level = getLevel(ti)
-			var si = ti.trim()
-			// ti = ti.replace(/\s/g, '&nbsp')
-      var left_length = (ti || '').length-(si || '').length
-      var space = ''
-      for (var s = 0; s <left_length; s++){
-        space += '&nbsp'
-      }
+            var r = "",
+                level = 0;
 
-			// r+='<ul><li class="x-space"><pre>'+space+'</pre></li><li class="x x'+level%5+'">'+si+'</li></ul>'
+            for (var i = 0; i < t.length; i++) {
+                // console.log(t[i])
+                var ti = (t[i]||'').replace(/(\s*$)/g, ''),
+                    si = ti.trim(),
+                    left_length = (ti || '').length - (si || '').length
+                if (si.indexOf('`') != 0) {
+                    level = getLevel(ti)
+                } else {
+                    si = si.replace(/\`+/, '')
+                }
+                r += '<span class="x x' + level % 5 + '" style="margin-left:' + left_length / 2 + 'em">' + si + '</span>'
 
-      // r+='<span class="x x'+level%5+'" style="margin-left:'+left_length/2+'em">'+si+'</span>'
-      r+='<span><span class="x">'+space+'</span><span class="x x'+level%5+'">'+si+'</span></span>'
+                // var space = ''
+                // for (var s = 0; s < left_length; s++) {
+                //   space += '&nbsp'
+                // }
+                // if(i){r+='<br/>'}
+                // r+='<span><span class="x">'+space+'</span><span class="x x'+level%5+'">'+si+'</span></span>'
 
-		}
-		p.replaceWith('<blockquote class="markx"><p>'+r+'</p></blockquote>')
-		// console.log( r)
-		// console.log(this)
-	}
-	)
+            }
+            p.replaceWith('<blockquote class="markx" oncopy="return false"><p>' + r + '</p></blockquote>')
+                // console.log( r)
+                // console.log(this)
+        }
+    )
 
 })()
